0b9d540c3c328b37e2ccf84ad0a1ad44bf6b9404
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2013-12-10 16:27:27 +0100

    FIX: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
-Spree::Core::Engine.routes.append do
-  match '/sofort/payment_network_callback' => 'checkout#payment_network_callback', :method => :get
+Spree::Core::Engine.routes.prepend do
+  match '/sofort/payment_network_callback', to: "checkout#payment_network_callback", via: [:get]
 end
